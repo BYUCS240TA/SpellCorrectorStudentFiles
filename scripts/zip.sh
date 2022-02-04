933ab94739f59fec53b9b5102f541224defbfb39
Spencer Hastings <me@spencerhastings.dev>
2022-02-04 14:09:31 -0700

    updated jars and action

--- scripts/zip.sh
+++ scripts/zip.sh
@@ -4,9 +4,7 @@ mkdir temp
 cp -r jars temp/jars
 cp -r passoff temp/passoff
 cp -r spell temp/spell
-cp notsobig.txt temp/notsobig.txt
-cp word.txt temp/word.txt
-cp words.txt temp/words.txt
+cp dictionaryFiles/*.txt temp
 
 cd temp
 zip -r SpellCorrectorStudentFiles.zip .
